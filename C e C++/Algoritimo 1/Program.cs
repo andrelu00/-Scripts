2199b32d559ef8449b0fc6d21c27dd9d3b0671ee
andrelu00 <andrelu0022@gmail.com>
2020-03-02 22:20:57 -0300

    Add files via upload

---
+++ C e C++/Algoritimo 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Algoritimo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Strings
+
+            string x1;
+            string y1;
+
+            string x2;
+            string y2;
+
+            //Respectivos int
+
+            int valx1;
+            int valy1;
+
+            int valx2;
+            int valy2;
+
+            double d;
+
+            Console.Write("Entre o valor de x no primeiro ponto: ");
+            x1 = Console.ReadLine();
+            valx1 = Convert.ToInt32(x1);
+
+            Console.Write("Entre o valor de y no primeiro ponto: ");
+            y1 = Console.ReadLine();
+            valy1 = Convert.ToInt32(y1);
+
+            Console.Write("Entre o valor de x no segundo ponto: ");
+            x2 = Console.ReadLine();
+            valx2 = Convert.ToInt32(x2);
+
+            Console.Write("Entre o valor de y no segundo ponto: ");
+            y2 = Console.ReadLine();
+            valy2 = Convert.ToInt32(y2);
+
+            d = (valx2 - valx1) * (valx2 - valx1) + (valy2 - valy1) * (valy2 - valy1);
+
+            Console.WriteLine("A distancia dos dois pontos é: " + Math.Sqrt(d));
+        }
+    }
+}
---
+++ C e C++/Algoritimo 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Algoritimo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A;
+            string B;
+            string C;
+
+            int ValA;
+            int ValB;
+            int ValC;
+            int R;
+            int S;
+            int D;
+
+
+            Console.WriteLine("Digite 3 Numeros inteiros: ");
+
+            Console.Write("Defina o Valor de A: ");
+            A = Console.ReadLine();
+            ValA = Convert.ToInt32(A);
+
+            if(ValA < 0)
+            {
+                Console.WriteLine("A precisa ser um numero positivo");
+            }
+
+            Console.Write("Defina o Valor de B: ");
+            B = Console.ReadLine();
+            ValB = Convert.ToInt32(B);
+
+            if (ValB < 0)
+            {
+                Console.WriteLine("B precisa ser um numero positivo");
+            }
+
+            Console.Write("Defina o Valor de C: ");
+            C = Console.ReadLine();
+            ValC = Convert.ToInt32(C);
+
+            if (ValC < 0)
+            {
+                Console.WriteLine("C precisa ser um numero positivo");
+            }
+
+            R = (ValA + ValB) * (ValA + ValB);
+            S = (ValB + ValC) * (ValB + ValC);
+
+            D = (R + S) / 2;
+
+            Console.WriteLine("O valor da operação é: " + D);
+
+        }
+    }
+}
