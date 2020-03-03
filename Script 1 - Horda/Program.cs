bc08a8682c68435b0ef47ad57b172f12b36e63ae
andrelu00 <andrelu0022@gmail.com>
2020-03-02 22:19:39 -0300

    Add files via upload

---
+++ Script 1 - Horda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Script_1___Horda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float NumHorda;
+            float VidaHorda = 30;
+            float AtaqueHorda = 10;
+            float NovaVidaHorda;
+            float NovoAtaqueHorda;
+            float Dificuldade;
+            float MultiplicadorDificuldade=25;
+
+
+            Dificuldade = MultiplicadorDificuldade / 100;
+
+            for (NumHorda = 0; NumHorda<=20; NumHorda++){
+                NovaVidaHorda = (VidaHorda * (NumHorda *(1 + Dificuldade))) + VidaHorda;
+                NovoAtaqueHorda = (AtaqueHorda * (NumHorda *(1 + Dificuldade))) + AtaqueHorda;
+
+                
+
+                Console.WriteLine("Horda: " + NumHorda + " ATK: " + NovoAtaqueHorda + " HP: " + NovaVidaHorda);
+            }
+        }
+    }
+}
