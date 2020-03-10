ac378f191c02d619ccc5f189faf561e0455c2263
andrelu00 <andrelu0022@gmail.com>
2020-03-10 11:53:42 -0300

    Add files via upload

---
+++ C e C++/Exercicio7-10do3/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int anonasc,anoatual,anofut;
+
+    anoatul=2020;
+    anofut=2030;
+
+    printf("IDADES \n");
+
+    printf("Digite seu ano de nascimento: \n");
+    scanf("%i" &anonasc);
+
+    printf("Sua idade atualmente: %i \n", )
+
+}
