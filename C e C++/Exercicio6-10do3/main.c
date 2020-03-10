7de24932b71d422797901de247db18ee03247201
andrelu00 <andrelu0022@gmail.com>
2020-03-10 11:43:51 -0300

    Add files via upload

---
+++ C e C++/Exercicio6-10do3/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n1,n2,p;
+
+    printf("Produto de dois numeros \n");
+
+    printf("Primero numero: ");
+    scanf("%i", &n1);
+
+    printf("Segundo numero: ");
+    scanf("%i", &n2);
+
+    p=n1*n2;
+
+    printf("Resultado: %i", p);
+
+
+}
